4ebb9530ee9cd3bfb5ce86e1a0b3c751e93d0276
Nikolay <example@gmail.com>
2021-12-18 04:47:55 +0200

    v10.0

--- script.js
+++ script.js
@@ -1,74 +1,88 @@
 /* Задание на урок:
 
-1) Первую часть задания повторить по уроку
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
 
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
 
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
 
 'use strict';
 
-let numberOfFilms;
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    start: function () {
+
+        this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+        while (this.count == '' || this.count == null || isNaN(this.count)) {
+            this.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = prompt('На сколько оцените его?', '');
+
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                this.movies[a] = b;
+                console.log('done');
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (this.count < 10) {
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (this.count >= 10 && this.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (this.count >= 30) {
+            console.log("Вы киноман");
+        } else {
+            console.log("Произошла ошибка");
+        }
+    },
+    writeYourGenres: function () {
+
+        let genresValue;
+
+        for (let i = 1; i <= 3; i++) {
+
+            do {
+                genresValue = prompt(`Ваш любимый жанр под номером ${i}`);
+            } while (genresValue == null || genresValue == '');
+
+            this.genres[i - 1] = genresValue;
+
+        }
+
+        this.genres.forEach(function (item, i) {
+            console.log(`Любимый жанр #${i + 1} - это ${item}`);
+        });
+    },
+    showMyDB: function () {
+        if (!this.privat) {
+            console.log(this);
+        }
+    },
+    toggleVisibleMyDB: function () {
+        if (this.privat === true) {
+            this.privat = false;
+        } else {
+            this.privat = true;
+        }
+    },
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: false,
 };
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-              b = prompt('На сколько оцените его?', '');
-    
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log('error');
-            i--;
-        }
-    }
-}
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
-}
-
-function writeYourGenres(obj) {
-    let answer = '';
-
-    for (let i = 0; i < 3; i++) {
-        obj[i] = prompt(`Ваш любимый жанр под номером ${i+1}:`);
-    }
-}
-
-
-function showMyDB(obj) {
-    obj == false ? console.log(obj) : console.log();
-}
-
-writeYourGenres(personalMovieDB.genres);
--- test.js
+++ test.js
@@ -1,1 +1,110 @@
 'use strict';
+
+const soldier = {
+    health: 400,
+    armor:  100,
+    damage: 50,
+    mana:   200,
+};
+
+const john = {
+    health: 100,
+    damage: 200,
+};
+
+john.__proto__ = soldier;
+
+console.dir(john);
+
+// let a = [1, 2, 4, 5],
+//     b = [12, 324, 345, 123];
+ 
+// let newArray = [...a, ...b, 213, 98, 97];
+
+// console.dir(newArray);
+
+
+
+// let arr = [10, 2, 3, 123, 5, 4, 643, 7, 8, 9, 21];
+// let leftPart = [];
+// let rightPart = [];
+
+
+// function sortQuick(arr) {
+
+//     let mid = Math.floor(arr.length / 2);
+    
+//     for (let i = 0; i <= mid; i++) {
+
+//         let j = mid + i;
+
+//         if (arr[i] > arr[j]) {
+
+//         } 
+        
+//     }
+
+//     console.log(arr);
+
+
+// }
+
+// sortQuick(arr);
+
+// arr.sort((a, b) => a - b);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+
+// arr.forEach(function(item, i, arr) {
+
+//     console.log(`${i}: ${item} внутри массива ${arr}`);
+
+// });
+
+// const options = {
+
+
+
+
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black', 
+//         bg: 'red'
+//     },
+//     makeTest: function () {
+//         console.log("Test");
+//     },
+// };
+
+// options.makeTest();
+
+// const {border, bg} = options.colors;
+
+// console.log(border);
+
+// console.log(Object.keys(options).length);
+
+// console.log(options.name);
+// delete options.name;
+// console.log(options);
+
+// let counter = 0;
+
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let prop in options[key]) {
+//             console.log(`Свойство ${prop} имеет значение ${options[key][prop]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// } 
+
+// console.log(counter);
