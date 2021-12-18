6c1434357ee894e11aed93be365a7b8539b0f3a0
Nikolay <example@gmail.com>
2021-12-18 05:07:02 +0200

    v10.2

--- elements/js/script.js
+++ elements/js/script.js
@@ -1,2 +1,25 @@
 'use strict';
 
+// const box = document.getElementById('box');
+
+// console.log(box);
+
+// const btns = document.getElementsByTagName('button');
+
+// console.log(btns);
+
+// const circles = document.getElementsByClassName('circle');
+
+// console.log(circles);
+
+// const hearts = document.querySelectorAll('.heart');
+
+// console.log(hearts);
+
+// hearts.forEach( item => {
+//     console.log(item);
+// });
+
+const oneHeart = document.querySelector('.heart');
+
+console.log(oneHeart);
